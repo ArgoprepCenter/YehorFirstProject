579ee2ab28f8dc7e560e84bf76c57fbb1d116a22
Yehor <karlinskyiyehor@gmail.com>
2024-10-19 12:24:09 -0400

    button experement

--- js/Asian/Pasta/script.js
+++ js/Asian/Pasta/script.js
@@ -1,4 +1,21 @@
 let button = document.getElementById("garlic-noodle");
 button.onclick = function(){
-    alert("hi");
+    modal.style.display = "block";
+}
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
