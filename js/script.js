b522b268ec6e901fc2e07c3b98c1cb0c5a598600
Vladimir Che <chevldmr@argoprepcenter.com>
2024-10-26 15:01:58 -0400

    test

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
-let button = document.getElementById("soy-sauce-pan-fried-noodles");
+let asianPastaRecipeElement = document.getElementById("asian-pasta-recipe");
 
-button.onclick = function(){
-    alert("Hi");
-};
+asianPastaRecipeElement.innerHTML = "Prep - 10 minutes mins</b>" +
+                                    "Cook - 15 minutes mins</b>";
+                                    // Total - 25 minutes mins
+                                    // Ingredients
+                                    // 1. 8 oz angel hair pasta 
+                                    // 2. 4 cloves garlic 
+                                    // 3. 1/2 bunch green onions 
+                                    // 4. 4 Tbsp butter 
+                                    // 5. 2 tsp soy sauce 
+                                    // 6. 2 Tbsp brown sugar 
+                                    // 7. 1 tsp sesame oil 
+                                    // 8. 2 Tbsp oyster sauce ";
