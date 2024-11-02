9d9999f37106158abac992fb376260136b6c40f4
Yehor <karlinskyiyehor@gmail.com>
2024-11-02 12:22:39 -0400

    recipes.js was created

--- js/Asian/Pasta/script.js
+++ js/Asian/Pasta/script.js
@@ -19,3 +19,17 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+let asianPastaRecipeElement = document.getElementById("asian-pasta-recipe");
+
+asianPastaRecipeElement.innerHTML = "Prep - 10 minutes mins<br />"+
+                                    "Cook - 15 minutes mins<br />"+
+                                    "Total - 25 minutes mins<br />"+
+                                    "Ingredients<br />"+
+                                    "1. 8 oz angel hair pasta<br />"+
+                                    "2. 4 cloves garlic<br />"+
+                                    "3. 1/2 bunch green onions<br />"+ 
+                                    "4. 4 Tbsp butter<br />"+
+                                    "5. 2 tsp soy sauce<br />"+ 
+                                    "6. 2 Tbsp brown sugar<br />"+
+                                    "7. 1 tsp sesame oil<br />"+
+                                    "8. 2 Tbsp oyster sauce<br />";
