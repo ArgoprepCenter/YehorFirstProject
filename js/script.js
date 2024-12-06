3f9b9d54919ee51374e63eda9d4e71b5f9fef5dd
Yehor <karlinskyiyehor@gmail.com>
2024-12-06 17:57:45 -0500

    script.js code changes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-let button = document.getElementsByClassName("btn");
-console.log(button);
+// let button = document.getElementsByClassName("btn");
+// console.log(button);
 
-for (let item of button) {
-    console.log(item.id);
-    item.onclick = function() {
-        // modal.style.display = "block";
-        // details = recipes.asian_pasta;
-        // recipeDetails.innerHTML = details;
-        recipe_id = item.id;
-        recipe_name = recipe_id.replace("-", "_");
-        console.log(recipes[recipe_name]);
-    }
-}
+// for (let item of button) {
+//     console.log(item.id);
+//     item.onclick = function() {
+//         // modal.style.display = "block";
+//         // details = recipes.asian_pasta;
+//         // recipeDetails.innerHTML = details;
+//         recipe_id = item.id;
+//         recipe_name = recipe_id.replace("-", "_");
+//         console.log(recipes[recipe_name]);
+//     }
+// }
