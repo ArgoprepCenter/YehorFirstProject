00dddd1e80f3b3c813582d567d3de8fb49d16880
Argo Center <chevldmr@gmail.com>
2024-12-13 17:50:24 -0500

    test

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ let header_id = document.getElementById("header_id");
 let button = document.getElementById("button");
 
 button.onclick = function(){header_id.style.backgroundColor = "red"};
+
+console.logf("Hello");
