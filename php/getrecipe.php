af3e028e0aaaa862ce6cbe1b88c94749a00a441b
Argo Center <chevldmr@gmail.com>
2025-01-31 17:25:12 -0500

    fixed error in getrecipe

--- php/getrecipe.php
+++ php/getrecipe.php
@@ -1,15 +1,16 @@
 <?php
   include ("dbconnect.php");
   $sql = "SELECT * FROM recipes Where name Like '%Pasta%'";
-  $results = mysqli_query($conn, $sql);
-  if (mysqli_num_rows($results) > 0) {
+  $result = mysqli_query($conn, $sql);
+  if (mysqli_num_rows($result) > 0) {
     $resultArray = array();
     $tempArray = array();
-    while($row = $result->fresh_object()) {
+    while($row = $result->fetch_object()) {
       $tempArray = $row;
       array_push($resultArray, $tempArray);
     }
     $result = json_encode($resultArray);
+    echo $result;
   } 
   else {
     echo "0 results";
